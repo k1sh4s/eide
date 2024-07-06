69beb3c36b31afc4d547ad15c430f5fbafa72bbc
Gerhard Eidenberger <gerhardgalaad@hotmail.com>
2024-07-06 11:06:49 -0300

    atualizacao para ver se o audio funciona

--- mypage.js
+++ mypage.js
@@ -15,6 +15,9 @@ function mudarAudio(meuAudio){
         var sourceUrl = "audio/" + meuAudio;
         theAudio.src = sourceUrl;
         theAudio.load();
+        theAudio.play().catch(error => {
+            console.error("Erro ao reproduzir o áudio:", error);
+        });
     } else {
         console.error("Elemento de áudio nao encontrado com o ID")
     }
@@ -31,16 +34,16 @@ var horarioLocal = new Date().getHours();
 
 if ((horarioLocal >= 9) && (horarioLocal <= 15)){
     document.body.style.backgroundImage = "url('imagem_rainworld/CycleDay.png')";
-    mudarAudio("audio/Threat_-_Heavy_Industrial.mp3");
+    mudarAudio("Threat_-_Heavy_Industrial.mp3");
     aCorQueQuero("cabeça", "black");
     aCorQueQuero("paragrafo", "black");
     aCorQueQuero("notinha", "black");
 } else if (((horarioLocal >= 5) && (horarioLocal < 9)) || ((horarioLocal > 15) && (horarioLocal <= 19))){
     document.body.style.backgroundImage = "url('imagem_rainworld/CycleSunset2.png')";
-    mudarAudio("audio/Kayava.mp3");
+    mudarAudio("Kayava.mp3");
 } else {
     document.body.style.backgroundImage = "url('imagem_rainworld/CycleNight.png')";
-    mudarAudio("audio/ELSE_IV.mp3");
+    mudarAudio("ELSE_IV.mp3");
     aCorQueQuero("cabeça", "#000080");
     aCorQueQuero("paragrafo", "#000080");
     aCorQueQuero("notinha", "#000080");
